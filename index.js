967caceb11430cfb9267a316cead8be471fdbe22
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2023-02-16 21:01:02 -0300

    improve player movement

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const canvasContext = canvas.getContext('2d')
 canvas.width = 1024 // 64 * 16
 canvas.height = 576 // 64 * 9
 
-
 const player1 = {
   color: 'red',
   height: 30,
@@ -17,65 +16,76 @@ const player1 = {
   ySpeed: 0,
   gravitySpeed: 0.2,
   jumpSpeed: 10,
-  movementSpeed: 10
+  movementSpeed: 5,
+  rightKeyDown: false,
+  leftKeyDown: false,
+  isJumping: false,
 }
 
-const drawPlayer = (player) => {
+const drawPlayer = player => {
   canvasContext.fillStyle = player.color
   canvasContext.fillRect(player.x, player.y, player.width, player.height)
 }
 
-const updatePlayer = (player) => {
+const playerJump = player => {
+  if (!player.isJumping) {
+    player1.ySpeed -= player1.jumpSpeed
+    player.isJumping = true
+  }
+}
+
+const updatePlayer = player => {
   player.y += player.ySpeed
   player.x += player.xSpeed
-  if(player.xSpeed > 0) {
-    player.xSpeed -= 1
-  } else if( player.xSpeed < 0){
+
+  if (player1.leftKeyDown) {
+    player1.xSpeed = -player1.movementSpeed
+  } else if (player.xSpeed < 0) {
     player.xSpeed += 1
+  }
+  if (player1.rightKeyDown) {
+    player1.xSpeed = player1.movementSpeed
+  } else if (player.xSpeed > 0) {
+    player.xSpeed -= 1
   }
 
   player.ySpeed += player.gravitySpeed
 
-
   // check up and down map constraints
-  if((player.height + player.y) > canvas.height) {
+  if (player.height + player.y > canvas.height) {
     player.y = canvas.height - player.height
+    player.isJumping = false
     player.ySpeed = 0
   } else if (player.y < 0) {
     player.y = 0
     player.ySpeed = 0
   }
 
-
   // check right and left map constraints
-  if((player.width + player.x) > canvas.width) {
+  if (player.width + player.x > canvas.width) {
     player.x = canvas.width - player.width
     player.xSpeed = 0
-  }else if(player.x < 0) {
+  } else if (player.x < 0) {
     player.x = 0
     player.xSpeed = 0
   }
 }
 
-
 const clearCanvas = () => {
   canvasContext.clearRect(0, 0, canvas.width, canvas.height)
 }
 
-const watchKeyBoardEvents = () => {
-  window.addEventListener('keydown', (event) => {
-    if(event.key == "a") {
-      player1.xSpeed -= player1.movementSpeed
-    } else if(event.key == "d") {
-      player1.xSpeed += player1.movementSpeed
-  } else if(event.key == " ") {
-    player1.ySpeed -= player1.jumpSpeed
-  } else {
-      console.log(event)
-    }
-  })
+const handleKeyboardEvents = event => {
+  if (event.key == 'a') {
+    player1.leftKeyDown = event.type == 'keydown'
+  }
+  if (event.key == 'd') {
+    player1.rightKeyDown = event.type == 'keydown'
+  }
+  if (event.key == ' ' && event.type == 'keydown') {
+    playerJump(player1)
+  }
 }
-
 
 const animateLoop = () => {
   clearCanvas()
@@ -84,5 +94,7 @@ const animateLoop = () => {
   window.requestAnimationFrame(animateLoop)
 }
 
-watchKeyBoardEvents()
+window.addEventListener('keydown', handleKeyboardEvents)
+window.addEventListener('keyup', handleKeyboardEvents)
+
 animateLoop()
